af48867fbe5168a785182a0d098541845291abe2
Emyr298 <emi.ru298@gmail.com>
2022-09-13 13:20:40 +0700

    feat: finish katalog app

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_catalog(request):
+    catalog_items = CatalogItem.objects.all()
+    context = {
+        'catalog_items': catalog_items,
+        'name': 'Emir Shamsuddin Fadhlurrahman',
+        'student_id': '2106632541',
+    }
+    
+    return render(request, 'katalog.html', context)

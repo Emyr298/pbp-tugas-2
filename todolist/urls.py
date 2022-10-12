47d91aff113b9337001735704d0a66b920e09225
Emyr298 <emi.ru298@gmail.com>
2022-10-12 21:00:16 +0700

    feat: use ajax

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-from todolist.views import delete_task, todolist, create_task, login_user, register, logout_user, set_finished, set_unfinished, delete_task
+from todolist.views import delete_task, delete_task_delete, todolist, create_task, login_user, register, logout_user, set_finished, set_unfinished, delete_task, todolist_json, create_task_post, delete_task_delete
 
 app_name = 'todolist'
 
 urlpatterns = [
     path('', todolist, name='todolist'),
     path('create-task/', create_task, name='create_task'),
+    path('add/', create_task_post, name='create_task_post'),
+    path('delete/<int:id>/', delete_task_delete, name='delete_task_delete'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', register, name='register'),
     path('set_finished/<int:id>/', set_finished, name='set_finished'),
     path('set_unfinished/<int:id>/', set_unfinished, name='set_unfinished'),
     path('delete_task/<int:id>/', delete_task, name='delete_task'),
+    path('json/', todolist_json, name='todolist_json'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,11 +1,13 @@
 import datetime
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from django.utils import timezone
 
 from todolist.models import Task
 
@@ -17,12 +19,49 @@ from todolist.models import Task
 @login_required(login_url='/todolist/login/')
 def todolist(request):
     user = request.user
-    task_list = Task.objects.filter(user__pk=user.pk)
+    #task_list = Task.objects.filter(user__pk=user.pk)
     context = {
         'username': user.username,
-        'task_list': task_list,
+        #'task_list': task_list,
     }
-    return render(request, 'todolist.html', context)
+    return render(request, 'todolist_ajax.html', context)
+
+@login_required(login_url='/todolist/login/')
+def todolist_json(request):
+    user = request.user
+    task_list = Task.objects.filter(user__pk=user.pk)
+    return HttpResponse(serializers.serialize('json', task_list), content_type='application/json')
+
+@login_required(login_url='/todolist/login/')
+def create_task_post(request):
+    user = request.user
+    
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        date = timezone.now()
+        
+        if title != '':
+            Task.objects.create(user_id=user.id, title=title, description=description, date=date)
+            
+            user = request.user
+            task_list = Task.objects.filter(user__pk=user.pk)
+            return HttpResponse(serializers.serialize('json', task_list), content_type='application/json')
+        else:
+            return JsonResponse({'message': 'Title tidak boleh kosong'}, status=400);
+    
+    return JsonResponse({'message': 'Not Found'}, status=404);
+
+@login_required(login_url='/todolist/login/')
+def delete_task_delete(request, id):
+    user = request.user
+    task = Task.objects.get(pk=id)
+    
+    if user.id == task.user_id and request.method == 'DELETE':
+        task.delete()
+        return JsonResponse({'message': 'Success'}, status=200);
+    
+    return JsonResponse({'message': 'Not Found'}, status=404);
 
 @login_required(login_url='/todolist/login/')
 def create_task(request):
